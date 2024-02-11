cd0185c18d79b0ccb8bc9f99a3f9be7548efe853
Akhil Trivedi <akhiltrivedi02021998@gmail.com>
2024-02-11 19:13:09 +0530

    usestate concept

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,42 +1,19 @@
 import RestaurentCard from "./RestaurentCard";
 import resList from "../utils/mockData";
-import { IMG_URL } from "../utils/constants";
-import { useState } from "react"; }
+import { useState } from "react";
 
 const Body = () => {
 
-  let listOfRestaurents = [
-    {
-      id: 1,
-      imageSrc: IMG_URL,
-      resName: 'Meghna Foods',
-      cuisine: ['Biryani', 'North Indian', 'Asian'],
-      stars: 3.2
-    },
-    {
-      id: 2,
-      imageSrc: IMG_URL,
-      resName: 'KFC',
-      cuisine: ['Burger', 'Chicken'],
-      stars: 4.2
-    },
-    {
-      id: 3,
-      imageSrc: IMG_URL,
-      resName: 'Dominos',
-      cuisine: ['Pizza'],
-      stars: 4.5
-    }
-  ]
+  const [listOfRestaurents, setListOfRestaurents] = useState(resList)
 
   return (
     <div className="body">
       <div className="filter">
         <button className="filter-btn" onClick={() => {
-          listOfRestaurents = listOfRestaurents.filter((res) => {
+          filteredList = listOfRestaurents.filter((res) => {
             return res.stars > 4.0;
           })
-          console.log(listOfRestaurents);
+          setListOfRestaurents(filteredList)
         }}>Top rated restaur</button>
       </div>
       <div className="res-container">
